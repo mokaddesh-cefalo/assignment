ad5159a6093cfa03517f8b7ee4dbf067521468b0
Mokaddesh Rashid Shovon <mokaddesh@cefalo.com>
2019-10-25 16:32:07 +0600

    added appConfig class to maintain DI and use factory

---
+++ src/main/java/com/cefalo/assignment/AppConfig.java
@@ -0,0 +1,9 @@
+package com.cefalo.assignment;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.cefalo.assignment")
+public class AppConfig {
+}
