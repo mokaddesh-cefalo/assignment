ec2b2736720bf72f7668f863413b64c3f0322793
mokaddesh-cefalo <mokaddesh@cefalo.com>
2019-10-26 22:37:29 +0600

    Added myUserDetailCLass

--- src/main/java/com/cefalo/assignment/service/business/MyUserDetailsService.java
+++ src/main/java/com/cefalo/assignment/service/business/MyUserDetailsService.java
@@ -26,45 +26,6 @@ public class MyUserDetailsService implements UserDetailsService {
        Optional<User> currentUser = userService.findUserByUserName(s);
 
        if(!currentUser.isPresent()) throw new  UsernameNotFoundException("No such user : " + s);
-       User user = currentUser.get();
-
-       return new UserDetails() {
-           @Override
-           public Collection<? extends GrantedAuthority> getAuthorities() {
-               return Arrays.stream(user.getRoles().split(","))
-                       .map(SimpleGrantedAuthority::new)
-                       .collect(Collectors.toList());
-           }
-
-           @Override
-           public String getPassword() {
-               return user.getPassword();
-           }
-
-           @Override
-           public String getUsername() {
-               return user.getUserName();
-           }
-
-           @Override
-           public boolean isAccountNonExpired() {
-               return true;
-           }
-
-           @Override
-           public boolean isAccountNonLocked() {
-               return true;
-           }
-
-           @Override
-           public boolean isCredentialsNonExpired() {
-               return true;
-           }
-
-           @Override
-           public boolean isEnabled() {
-               return true;
-           }
-       };
+       return currentUser.map(MyUserDetails::new).get();
     }
 }
