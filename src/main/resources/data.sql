676cbc1e8f6614dcc5a71048af3714ef3960d9bb
Mokaddesh Rashid Shovon <mokaddesh@cefalo.com>
2019-11-07 16:34:44 +0600

    added data.sql to insert data on initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into user values ('fish', NULL, '2019-11-07 16:22:02.716770', NULL, 'pass', 'ROLE_USER');
+insert into user values ('i', NULL, '2019-11-07 16:22:02.716770', NULL, 'love', 'ROLE_USER');
+
+insert into story values
+    ( '1', 'body-1', '2019-11-07 16:22:21.182889', NULL, '2018-07-15 06:00:00.000000', 't-1', 'fish');
+insert into story values
+( '2', 'body-2', '2019-11-07 16:22:21.182889', NULL, '2015-07-15 06:00:00.000000', 't-2', 'i');
+insert into story values
+( '3', 'body-3', '2019-11-07 16:22:21.182889', NULL, '2010-07-15 06:00:00.000000', 't-3', 'fish');
+insert into story values
+( '4', 'body-4', '2019-11-07 16:22:21.182889', NULL, '2019-07-15 06:00:00.000000', 't-4', 'i');
+insert into story values
+( '5', 'body-5', '2019-11-07 16:22:21.182889', NULL, '2018-07-25 06:00:00.000000', 't-5', 'fish');
+insert into story values
+( '6', 'body-6', '2019-11-07 16:22:21.182889', NULL, '2018-01-15 06:00:00.000000', 't-6', 'i');
